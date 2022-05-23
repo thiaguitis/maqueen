0c6bd26bfe71171d73bb6a1e6cc4fa028f195637
thiaguitis <99951132+thiaguitis@users.noreply.github.com>
2022-05-23 13:17:22 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     basic.pause(2000)
-    maqueen.motorStop(maqueen.Motors.M2)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
     basic.pause(2000)
-    maqueen.motorStop(maqueen.Motors.M1)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     basic.pause(2000)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+})
+input.onButtonPressed(Button.B, function () {
+	
 })
 basic.showString("Rayo Mqueen Cachau")
 basic.showLeds(`
