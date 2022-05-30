e059474e2273ff739a90e5907bb2653a07002f55
thiaguitis <99951132+thiaguitis@users.noreply.github.com>
2022-05-30 13:11:58 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,9 +8,28 @@ input.onButtonPressed(Button.A, function () {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
     basic.pause(2000)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+    basic.pause(2000)
+    maqueen.motorStop(maqueen.Motors.All)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    basic.pause(5000)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    basic.pause(100)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    basic.pause(5000)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    basic.pause(100)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    basic.pause(5000)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    basic.pause(100)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    basic.pause(5000)
+    maqueen.motorStop(maqueen.Motors.All)
 })
 basic.showString("Rayo Mqueen Cachau")
 basic.showLeds(`
@@ -20,6 +39,3 @@ basic.showLeds(`
     . # # # .
     . # # # .
     `)
-basic.forever(function () {
-	
-})
